46d8ba11a6adb8f4f42fa390505be2e953c8ab02
Marcelearn <jaworskimarcel@gmail.com>
2022-10-26 15:23:41 +0200

    navbar styled

--- src/components/Mynavbar.js
+++ src/components/Mynavbar.js
@@ -1,18 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 function Mynavbar() {
     return (
       <Navbar bg="dark" variant="dark">
-        <h1 bg="dark" variant="dark">Rado's Blog</h1>
         <Container>
-            
-          <Navbar.Brand href="#home">HOME</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Signed in as: <a href="#login">Mark Otto</a>
-            </Navbar.Text>
-          </Navbar.Collapse>
+            <Navbar.Brand href="#home">Rado's Blog</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="#home">HOME</Nav.Link>
+                <Nav.Link href="#management">MANAGEMENT</Nav.Link>
+                <Nav.Link href="#graphql">GRAPHQL</Nav.Link>
+                <Nav.Link href="#react">REACT</Nav.Link>
+                <Nav.Link href="#ruby">RUBY ON RAILS</Nav.Link>
+            </Nav>
+            <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>
+                Signed in as: <a href="#login">Marcel</a>
+                </Navbar.Text>
+            </Navbar.Collapse>
         </Container>
       </Navbar>
     );
